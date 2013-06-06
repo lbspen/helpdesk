f382b3e8904bd82c3220700a3d54319608d38472
lbs <lbspen@gmail.com>
2013-06-06 14:26:33 -0700

    initial app generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpdesk::Application.config.secret_token = '1a1cd18d94970b267434c41fb0ff9bf8099773177123b81b9a49ec839c88cc63e431643813353f7011d5762671bb2482acb544264e8d0b69f057251c65368a37'
