6672fb961982423dd94a22b810a050477dc93b2a
lbs <lbspen@gmail.com>
2013-06-07 22:01:07 -0700

    Sends email to helpdesk when ticket is created

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,7 @@ ActionMailer::Base.smtp_settings = {
   user_name: ENV["SENDGRID_USERNAME"],
   password: ENV["SENDGRID_PASSWORD"],
   domain: "example.com",
+  address: "smtp.sendgrid.net",
   port: 587,
   authentication: :plain,
   enable_starttls_auto: true
