680bd355be945e221d546f1d6591d94b00d0de4b
lbs <lbspen@gmail.com>
2013-07-05 12:32:53 -0700

    debug

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,7 +1,9 @@
-class IncomingMailsController < ApplicationController    
+class IncomingMailsController < ApplicationController
+  require 'mail'
   skip_before_filter :verify_authenticity_token
 
   def create
+    logger.info "incomingmailscontroller#create params[:message]: #{params[:message]}"
     ticket = TicketMailer.receive(Mail.new(params))
     if (ticket)
       render :text => 'success', :status => 200
