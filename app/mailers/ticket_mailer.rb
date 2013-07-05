4372032501fbe45f74fb51f4906cbd4be96b84ca
lbs <lbspen@gmail.com>
2013-07-04 20:52:29 -0700

    debug

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -18,6 +18,7 @@ class TicketMailer < ActionMailer::Base
 
   def receive(email)
     logger.info "TicketMailer - received an email"
+    logger.info email
     @@lastReceipt = email
     ticket_id = /Ticket: (\d*)/.match(email.subject)
     if ticket_id && $1
